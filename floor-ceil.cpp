9383748e105788d14711867a7dc45759a737f0a4
amanagarwalcse27 <amanagarwal192003@gmail.com>
2025-07-06 09:21:58 +0530

    Add files via upload

--- floor-ceil.cpp
+++ floor-ceil.cpp
@@ -1,3 +1,9 @@
+//floor of element is largest elemnt  which is lesser or equal to key, uske equal ya usse just chhota element present in array
+ // eg  1 2 4 8 10 10 12    9 ka floor 8 h ,10 ka floor 10 h ,  12 ka floor ,7 ka floor 4 h   
+
+ //ceil of elemnt is smallest elemnt which is greater or equal to key
+ //eg  1  2 4 8 10 10 12  9 ka ceil 10 h ,2 ka ceil 2 h is array me 
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -11,16 +17,16 @@ int floor(int a[],int n ,int k)
             int mid = start + (end - start)/2;
             if(a[mid] == k)
             {
-                floor = mid;
-                break;
+                floor = mid;  //possible answer ho skta h
+                start = mid+1; //for multiple occurence
             }
-            else if(a[mid] > k)
+            else if(a[mid] > k)  //agr given element less ho k se toh left part me srch krenge
             {
               end = mid -1;  
             }
-            else
+            else if(a[mid] < k)  //agr a[mid] less than a[mid] se toh yh possible answer ho skta h isko store krke right me dhundenge
             {
-                floor = mid;
+                floor = mid;  //possible answer h then space mmoves to right greater than it
                 start = mid + 1;
             }
         }
@@ -40,7 +46,7 @@ int ceil(int a[],int n, int k)
             if(a[mid] == k)
             {
                 ceil = mid;
-                break;
+                end = mid-1;
             }
             else if(a[mid] > k)
             {
@@ -58,7 +64,7 @@ int ceil(int a[],int n, int k)
 
 int main()
 {
-    int a[] = { 1,2,4,8,10,12};
+    int a[] = { 1,2,4,8,10,10,12};  //for multiple occurences of floor return last occurence index
     int n = sizeof(a)/sizeof(a[0]);
     int k ;
     cin>>k;
